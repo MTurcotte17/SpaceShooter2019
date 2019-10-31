f0a750625176bf37fecc6762ded74c2136d4cc47
Julien Pelchat <minetro30@gmail.com>
2019-10-30 20:18:04 -0400

    pushing my part for the moment first part of the mainmenu done

---
+++ SpaceShooterUnity/Assets/Scenes/Scripts/PlayingChoice.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class PlayingChoice : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_ButtonChoice;
+
+    [SerializeField]
+    private GameObject m_Image;
+
+    public void OpenChoice(int aChoice)
+    {
+        if(aChoice == 1)
+        {
+            m_Image.SetActive(false);
+            m_ButtonChoice.SetActive(true);
+        }
+    }
+}
